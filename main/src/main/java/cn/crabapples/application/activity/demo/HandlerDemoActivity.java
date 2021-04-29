e6b111639515e603a252de34c0a6f7934c8b09e6
hequan <crabapples.cn@gmailcom>
2021-04-30 02:15:04 +0800

    handler demo thread message

--- main/src/main/java/cn/crabapples/application/activity/demo/HandlerDemoActivity.java
+++ main/src/main/java/cn/crabapples/application/activity/demo/HandlerDemoActivity.java
@@ -11,29 +11,38 @@ import cn.crabapples.application.R;
 public class HandlerDemoActivity extends AppCompatActivity {
     private final String TAG = "HandlerDemoActivity";
     private TextView textView;
+    Handler.Callback callback;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.handler);
         textView = findViewById(R.id.content);
+        callback = msg -> {
+            try {
+                Thread.sleep(2000);
+                printLog(msg.obj.toString() + Thread.currentThread().getName());
+                textView.setText(msg.obj.toString());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return false;
+        };
+        handler = new Handler(Looper.myLooper(), callback);
     }
 
     public void send(View view) {
-        Handler.Callback callback = msg -> {
-            printLog(msg.obj.toString());
-            textView.setText(msg.obj.toString());
-            return false;
-        };
-        Handler handler = new Handler(Looper.myLooper(), callback);
-        Message message = handler.obtainMessage();
-        String content = "handler测试";
-        message.what = 1;
-        message.obj = content;
-        handler.sendMessage(message);
-
-        printLog(content);
+        new Thread(() -> {
+            Message message = handler.obtainMessage();
+            String content = "handler测试";
+            message.what = 1;
+            message.obj = content;
+            handler.sendMessage(message);
+            printLog(content + Thread.currentThread().getName());
+        }).start();
     }
+    class MyHandler extends Handler{}
 
     public void back(View view) {
     }
