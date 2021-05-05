f842a5122b8328415ab20cd23e02dc1f9dd4e549
hequan <>
2021-05-06 01:39:28 +0800

    update file name

--- main/src/main/java/cn/crabapples/application/activity/demo/PopupWindowActivity.java
+++ main/src/main/java/cn/crabapples/application/activity/demo/PopupWindowActivity.java
@@ -3,9 +3,9 @@ package cn.crabapples.application.activity.demo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.PopupWindow;
 import android.widget.Toast;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import cn.crabapples.application.R;
 
@@ -15,15 +15,25 @@ public class PopupWindowActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.popup_window);
+        setContentView(R.layout.activity_popup_window);
     }
 
     public void showPopup(View view) {
-        PopupWindow popupWindow = new PopupWindow(PopupWindowActivity.this);
+        printLog("showPopup()");
+        View layout = getLayoutInflater().inflate(R.layout.module_popup_window, null);
+        PopupWindow popupWindow = new PopupWindow(layout,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                true);
+        popupWindow.showAsDropDown(view, view.getLayoutParams().width, view.getLayoutParams().height);
     }
 
     public void showToast(String content) {
         Toast.makeText(PopupWindowActivity.this, content, Toast.LENGTH_SHORT).show();
+    }
+
+    public void printLog(String content) {
+        Log.i(TAG, content);
     }
 
     public void back(View view) {
